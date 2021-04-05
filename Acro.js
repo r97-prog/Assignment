08e4cc711eeaaa3f066252891684bc75dbafad62
r97-prog <79899417+r97-prog@users.noreply.github.com>
2021-04-05 23:43:39 +0530

    Assignment

---
+++ Acro.js
@@ -0,0 +1,20 @@
+console.log("rishabh");
+
+function formData(){
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var username = document.getElementById("userName").value;
+    var city = document.getElementById("city").value;
+    var state = document.getElementById("state").value;
+    var zip = document.getElementById("zip").value;
+    console.log(firstName+" "+lastName+" "+username+" "+city+" "+zip+" "+state);
+
+
+};
+
+function formData(){
+    e.preventdefault();
+    var Email = document.getElementById("Email").value;
+    var password = document.getElementById("Password").value;
+    console.log(Email+""+Password);
+}
